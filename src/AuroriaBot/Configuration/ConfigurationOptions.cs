bbc0631f580dcc662d576275f8f6b121c61605e7
Tom De Smedt <tom_dsmedt@hotmail.com>
2020-10-08 22:54:25 +0200

    - Initial Discord bot code with login logging - Added Configuration builder

---
+++ src/AuroriaBot/Configuration/ConfigurationOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuroriaBot.Configuration
+{
+    public class ConfigurationOptions
+    {
+        public string Token { get; set; }
+    }
+}
---
+++ src/AuroriaBot/Program.cs
@@ -0,0 +1,61 @@
+﻿using AuroriaBot.Configuration;
+using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
+
+namespace AuroriaBot
+{
+    class Program
+    {
+        public static ConfigurationOptions ConfigurationOptions { get; private set; }
+        private DiscordSocketClient _client;
+
+        public static void Main(string[] args) {
+            CreateHostBuilder(args).Build().RunAsync();
+
+            new Program().MainAsync().GetAwaiter().GetResult();
+        }
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+
+            var token = ConfigurationOptions.Token;
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, configuration) =>
+                {
+                    configuration.Sources.Clear();
+
+                    IHostEnvironment env = hostingContext.HostingEnvironment;
+
+                    configuration
+                        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
+                    IConfigurationRoot configurationRoot = configuration.Build();
+
+                    ConfigurationOptions = new ConfigurationOptions();
+                    configurationRoot.Bind(ConfigurationOptions);
+                });
+    }
+}
